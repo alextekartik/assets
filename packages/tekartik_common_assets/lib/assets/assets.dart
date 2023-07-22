b86a2e284fd7e51ff65a481c2b4ef2310d28132b
Alexandre Roux <alex@tekartik.com>
2023-07-22 15:04:53 +0200

    play button

---
+++ packages/tekartik_common_assets/lib/assets/assets.dart
@@ -0,0 +1,4 @@
+const playButtonIconWhite512 = 'play_button_icon_white_512x512.png';
+const playButtonMaskableIconWhite512 =
+    'play_button_maskable_icon_white_512x512.png';
+const playButtonWhite512 = 'play_button_white_512x512.png';
